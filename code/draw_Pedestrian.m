4685122e1cd0e4a9f8f1a82575625ad92603046c
Stefan Zoller <stzoller@student.ethz.ch>
2012-04-23 18:40:19 +0200

    Funktion zum Zeichnen der Personen erstellt

---
+++ code/draw_Pedestrian.m
@@ -0,0 +1,21 @@
+% Modeling and Simulating Social Systems with MATLAB
+% BlueMen - Pedestrian Dynamics
+% Dominic Hänni, Patrick Manser, Stefan Zoller
+% Author: Stefan Zoller, 2012
+
+function draw_Pedestrian(x0, y0, r);
+% This function is drawing a Pedestrian
+% INPUT: 
+%   x0, y0: Central point of the Pedestrian
+%   r: Radius of the Pedestrian
+
+
+hold on
+
+% Define the coordinates for the Pedestrian
+angles = 0:0.1:(2*pi);
+Pedestrian_x = r*cos(angles);
+Pedestrian_y = r*sin(angles);
+
+% Draw the Pedestrian
+patch(x0+Pedestrian_x, y0+Pedestrian_y, 'b')
